33c4dc40c39a7ad2d278949a87705e1ac509e598
Storm <wenyejie@foxmail.com>
2018-10-26 12:00:43 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,17 @@ module.exports = {
     sourceMap: true
   },
 
-  lintOnSave: undefined
+  lintOnSave: 'error',
+
+  devServer: {
+    port: 8888,
+    proxy: {
+      '/api': {
+        target: 'http://test.zhiqicloud.com:8770/',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: { '^/api': '/' }
+      }
+    }
+  }
 }
