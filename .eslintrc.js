354041edcc85b779d22b17ec6766d377a5daaaaa
Storm <wenyejie@foxmail.com>
2018-10-26 12:17:37 +0800

    调整eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,17 @@ module.exports = {
   ],
   rules: {
     'no-console': 'off',
-    'no-debugger': 'off'
+    'no-debugger': 'off',
+    'vue/attributes-order': 'error',
+    'vue/html-quotes': 'error',
+    'vue/no-confusing-v-for-v-if': 'error',
+    'vue/order-in-components': 'error',
+    'vue/this-in-template': 'error',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/html-closing-bracket-spacing': 'error',
+    'vue/no-use-v-if-with-v-for': 'error',
+    'vue/prop-name-casing': 'error',
+    'vue/script-indent': 'error'
   },
   parserOptions: {
     parser: 'typescript-eslint-parser'
