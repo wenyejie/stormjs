632fee811af6087447328f73ed849d9630a946d2
Storm <wenyejie@foxmail.com>
2018-10-26 11:57:15 +0800

    调整eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,13 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
     '@vue/standard',
     '@vue/typescript'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off'
   },
   parserOptions: {
     parser: 'typescript-eslint-parser'
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  runtimeCompiler: true,
+
+  css: {
+    modules: true,
+    sourceMap: true
+  },
+
+  lintOnSave: undefined
+}
