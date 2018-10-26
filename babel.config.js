0c80321404a356590b9ef1dfd2759477302e9e1a
Storm <wenyejie@foxmail.com>
2018-10-26 11:47:44 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    '@vue/app', {
+      polyfills: [
+        'es6.promise',
+        'es6.object.assign',
+        'es6.object.keys',
+        'es6.array.filter',
+        'es6.array.map',
+        'es6.array.find',
+        'es7.array.includes',
+        'es7.symbol.async-iterator'
+      ]
+    }
   ]
 }
