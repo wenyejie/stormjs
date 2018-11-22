131174b31ae653ca4bb2221c4170306b0f2e9b17
Storm <wenyejie@foxmail.com>
2018-11-22 10:05:31 +0800

    新增默认值

--- src/utils/dateConvert.js
+++ src/utils/dateConvert.js
@@ -6,11 +6,11 @@
  * @date: 2018/05/10
  */
 
-import isDate from './isDate'
+import isValidDate from './isValidDate'
 
-export default (date) => {
-  if (isDate(date)) return date
-  if (!date) return
+export default (date, defDate) => {
+  if (isValidDate(date)) return date
+  if (!date) return defDate
 
   // 如果是纯数字的字符串则转换成number类型
   if (typeof date === 'string' && /^\d+$/.test(date)) date = parseInt(date)
@@ -19,16 +19,16 @@ export default (date) => {
     if ((date + '').length > 13) date = parseInt((date + '').substr(0, 13))
 
     // is NaN
-    if (Number.isNaN(date)) return
+    if (Number.isNaN(date)) return defDate
 
     // 小于秒级时间戳
-    if (date / 1e9 < 1) return
+    if (date / 1e9 < 1) return defDate
 
     // 秒级时间戳, 转换成毫秒级时间戳
     if (date / 1e9 > 1 && (date + '').length === 10) date = date * 1e3
   }
   date = new Date(date)
 
-  if (date.toString() === 'Invalid Date') return
+  if (date.toString() === 'Invalid Date') return defDate
   return date
 }
