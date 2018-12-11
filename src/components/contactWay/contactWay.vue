12284d8a70e85dd05cb31b4e79ebc957e5bbf779
z54749412 <zhuangguanxue@163.com>
2018-12-11 09:58:18 +0800

    联系方式组件完成

---
+++ src/components/contactWay/contactWay.vue
@@ -0,0 +1,79 @@
+<!--
+ * @Description: 联系方式组件 showvalue 为展示的联系方式 type 为匹配的对应icon 现有规则为['DH', 'DD', 'WX']
+ * @Author: Ntfs
+ * @Date: 2018-12-10 15:58:44
+ * @LastEditors: Ntfs
+ * @LastEditTime: 2018-12-11 09:54:13
+ -->
+
+<template>
+  <div
+    class="contact"
+    :type="type"
+    :showvalue="showvalue">
+    <a
+      v-if="type==='QQ'"
+      target="_blank"
+      :href="`http://wpa.qq.com/msgrd?v=3&uin=${showvalue}&site=qq&menu=yes`"
+      :style="{'font-size': '14px', 'color': '#444444'}">
+      <s-icon
+        :size="20"
+        color="info"
+        :style="{'border-radius': '50%', 'margin-right': '4px'}"
+        :type="'qqx'" />
+      {{ showvalue }}
+    </a>
+    <span
+      v-else
+      :style="{'font-size': '14px', 'color': '#444444'}">
+      <s-icon
+        :size="20"
+        color="info"
+        :style="{'border-radius': '50%', 'margin-right': '4px'}"
+        :type="icon.indexOf(type) > -1 ? iconFont[icon.indexOf(type)] : 'warning'" />
+      {{ showvalue }}
+    </span>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SContactWay',
+  props: {
+    showvalue: {
+      type: Number | null | String,
+      default: null
+    },
+    type: {
+      type: null | String,
+      default: null
+    }
+  },
+  data () {
+    return {
+      icon: ['DH', 'DD', 'WX'],
+      iconFont: ['dianhuax', 'dingdingx', 'weixinx']
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  .contact {
+    display: flex;
+    // justify-content: center;
+    align-items: center;
+    padding: 0 2px;
+    >a,span {
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
---
+++ src/components/contactWay/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 联系方式组件
+ * @Author: Ntfs
+ * @Date: 2018-12-10 15:59:44
+ * @LastEditors: Ntfs
+ * @LastEditTime: 2018-12-10 15:59:52
+ */
+
+export default () => import('./contactWay.vue')
